6073ef401cf19342ae50b0a5c216ee6ca5f16fcd
Ari Diah <aridiah271@gmail.com>
2024-11-28 12:42:58 +0800

    Create a design in xml (Home, News, and Scan History)

--- app/src/main/java/com/example/reto/MainActivity.kt
+++ app/src/main/java/com/example/reto/MainActivity.kt
@@ -2,17 +2,43 @@ package com.example.reto
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.reto.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        // Binding layout
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        bottomNavigationView.background = null
-        bottomNavigationView.menu.getItem(2).isEnabled = false
+        val navView: BottomNavigationView = binding.bottomNavigationView
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_knowledge,
+                R.id.navigation_scan,
+                R.id.navigation_news,
+                R.id.navigation_history
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        navView.background = null
+        navView.menu.getItem(2).isEnabled = false
 
         supportActionBar?.hide()
     }
